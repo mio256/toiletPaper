03c651868e70f20b8aea606c526baf93739f7ea7
mio256 <nao.reiya0215@gmail.com>
2021-09-12 13:59:35 +0900

    address win

--- SearchDir.java
+++ SearchDir.java
@@ -3,12 +3,13 @@ import java.io.File;
 public class SearchDir {
     public static void main(String[] args) {
 
-        String path1 = "/home/skyadmin/toiletPaper/data";
+        // String path1 = "/home/skyadmin/toiletPaper/data";
+        String path1 = "C:\\toiletPaper\\data\\";
 
         File file1 = new File(path1);
         File fileArray1[] = file1.listFiles();
 
-        String[] toiletComboData = {"toilet1", "toilet2", "toilet3", "toilet4", "toilet5"};
+        String[] toiletComboData = { "toilet1", "toilet2", "toilet3", "toilet4", "toilet5" };
         int[] sum = new int[toiletComboData.length];
         // ファイルの一覧
         for (File f : fileArray1) {
