42b94dde5eac900fdc3dedf708271d1c0c5b6f0c
mio256 <nao.reiya0215@gmail.com>
2021-09-12 13:40:04 +0900

    場所別の消費数

--- SearchDir.java
+++ SearchDir.java
@@ -8,15 +8,25 @@ public class SearchDir {
         File file1 = new File(path1);
         File fileArray1[] = file1.listFiles();
 
-        Integer sum = 0;
+        String[] toiletComboData = {"toilet1", "toilet2", "toilet3", "toilet4", "toilet5"};
+        int[] sum = new int[toiletComboData.length];
         // ファイルの一覧
         for (File f : fileArray1) {
             if (f.isFile()) {
                 System.out.println(f.toString());// ファイルを表示
                 Seri search = new Seri();
-                sum += search.readNum(f.toString());
+                for (int i = 0; i < toiletComboData.length; i++) {
+                    if (search.readText(f.toString()).equals(toiletComboData[i])) {
+                        sum[i] += search.readNum(f.toString());
+                    }
+                }
             }
         }
-        System.out.println("合計数:" + sum);// 合計表示
+        int sums = 0;
+        for (int i = 0; i < toiletComboData.length; i++) {
+            System.out.println(toiletComboData[i] + "の消費数:" + sum[i]);
+            sums += sum[i];
+        }
+        System.out.println("全体の消費数:" + sums);
     }
 }
